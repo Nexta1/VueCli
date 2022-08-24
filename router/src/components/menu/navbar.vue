ba8c093524497de9a922e97625f8b04b74378635
nexta1 <798868370@qq.com>
2022-08-24 11:33:04 +0800

    login

--- router/src/components/menu/navbar.vue
+++ router/src/components/menu/navbar.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-
-    </div>
+  <div >
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">promotion management</a></el-breadcrumb-item>
+      <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+      <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- router/src/layouts/admin.vue
+++ router/src/layouts/admin.vue
@@ -2,6 +2,7 @@
   <div class="flex w-screen h-screen">
     <MenuComponent />
     <div class="right flex-1 bg-zinc-300">
+      <Navbar />
       <router-view />
     </div>
   </div>
@@ -9,6 +10,7 @@
 
 <script setup lang="ts">
 import MenuComponent from '../components/menu/menu.vue'
+import Navbar from '../components/menu/navbar.vue'
 </script>
 
 <style scoped></style>
